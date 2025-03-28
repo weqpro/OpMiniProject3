34cf2f3a70c4c44cb4872b3315cf1e3a09d2731a
weqpro <weqpro@gmail.com>
2025-03-28 11:25:19 +0200

    feat: created repositoryContext, base declarative model for SQLAlchemy
    fix: docker-compose and pyproject.toml

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
-from typing import Union
 from fastapi import FastAPI
+from app.repository.repository_context import RepositoryContext
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
+@app.get("/")
+async def read_root():
+    _ = RepositoryContext()
+    return {"Hello": "World"}
