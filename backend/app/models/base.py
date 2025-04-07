812ff18ca94837ba979be3405867b1a1789e7781
Sofiia-Pereima <pereima.pn@ucu.edu.ua>
2025-04-07 19:05:23 +0300

    Update base.py

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,9 +1,9 @@
-# from sqlalchemy.ext.asyncio import AsyncAttrs
-# from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
 
 
-# class Base(AsyncAttrs, DeclarativeBase): ...
+class Base(AsyncAttrs, DeclarativeBase): ...
 
-from sqlalchemy.ext.asyncio import AsyncAttrs, declarative_base
+# from sqlalchemy.ext.asyncio import AsyncAttrs, declarative_base
 
-Base = declarative_base()
+# Base = declarative_base()
