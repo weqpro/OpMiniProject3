0a47807a4df25121f94a1c3b665ba7a34244ce3d
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-04-08 13:19:51 +0300

    aid request service update

--- backend/app/crud/aid_request.py
+++ backend/app/crud/aid_request.py
@@ -2,15 +2,15 @@ from sqlalchemy.orm import Session
 from backend.app import models, schemas
 import datetime
 
-def create_aid_request(db: Session, aid_request: schemas.AidRequestCreate):
-    db_aid_request = models.AidRequest(**aid_request.dict())
+def create_aid_request(db: Session, aid_request: schemas.aid_request.AidRequestCreate):
+    db_aid_request = models.aid_request.AidRequest(**aid_request.dict())
     db.add(db_aid_request)
     db.commit()
     db.refresh(db_aid_request)
     return db_aid_request
 
-def update_aid_request_status(db: Session, aid_request_id: int, status: models.AidRequestStatus):
-    db_aid_request = db.query(models.AidRequest).filter(models.AidRequest.id == aid_request_id).first()
+def update_aid_request_status(db: Session, aid_request_id: int, status: models.aid_request.AidRequestStatus):
+    db_aid_request = db.query(models.aid_request.AidRequest).filter(models.aid_request.AidRequest.id == aid_request_id).first()
     if db_aid_request:
         db_aid_request.status = status
         db.commit()
@@ -19,7 +19,7 @@ def update_aid_request_status(db: Session, aid_request_id: int, status: models.A
 
 
 def set_volunteer_deadline(db: Session, aid_request_id: int, deadline: datetime.datetime):
-    db_aid_request = db.query(models.AidRequest).filter(models.AidRequest.id == aid_request_id).first()
+    db_aid_request = db.query(models.aid_request.AidRequest).filter(models.aid_request.AidRequest.id == aid_request_id).first()
     if db_aid_request:
         db_aid_request.volunteer_deadline = deadline
         db.commit()
@@ -27,7 +27,7 @@ def set_volunteer_deadline(db: Session, aid_request_id: int, deadline: datetime.
     return db_aid_request
 
 def delete_aid_request(db: Session, aid_request_id: int, soldier_id: int):
-    aid_request = db.query(models.AidRequest).filter(models.AidRequest.id == aid_request_id, models.AidRequest.soldier_id == soldier_id).first()
+    aid_request = db.query(models.aid_request.AidRequest).filter(models.aid_request.AidRequest.id == aid_request_id, models.aid_request.AidRequest.soldier_id == soldier_id).first()
     
     if aid_request:
         db.delete(aid_request)
@@ -36,11 +36,11 @@ def delete_aid_request(db: Session, aid_request_id: int, soldier_id: int):
     return False
 
 def reject_aid_request(db: Session, aid_request_id: int, volunteer_id: int):
-    aid_request = db.query(models.AidRequest).filter(models.AidRequest.id == aid_request_id).first()
+    aid_request = db.query(models.aid_request.AidRequest).filter(models.aid_request.AidRequest.id == aid_request_id).first()
     
     if aid_request:
         if volunteer_id in [volunteer.id for volunteer in aid_request.volunteers]:
-            aid_request.status = 'Очікування'
+            aid_request.status = models.aid_request.AidRequestStatus.PENDING
             db.commit()
             return True
     return False
--- backend/app/services/aid_request_service.py
+++ backend/app/services/aid_request_service.py
@@ -1,9 +1,12 @@
 from typing import List
 
-from app.models.aid_request import AidRequest
-from app.repository.repository_manager import RepositoryManager
-from app.repository.aid_request_repository import AidRequestRepository
-from app.schemas.search_options import SearchOptions
+from backend.app.models.aid_request import AidRequest
+from backend.app.repository.repository_manager import RepositoryManager
+from backend.app.repository.aid_request_repository import AidRequestRepository
+from backend.app.schemas.search_options import SearchOptions
+from backend.app.schemas.aid_request import AidRequestCreate, AidRequestOut
+from backend.app.models.aid_request import AidRequestStatus
+import datetime
 
 
 class AidRequestService:
@@ -18,3 +21,18 @@ class AidRequestService:
         self, search_options: SearchOptions | None = None
     ) -> List[AidRequest]:
         return [AidRequest.create_dummy(), AidRequest.create_dummy()]
+    
+    async def create_aid_request(self, db, aid_request: AidRequestCreate) -> AidRequestOut:
+        return await self.__repository.create_aid_request(db=db, aid_request=aid_request)
+
+    async def update_aid_request_status(self, db, aid_request_id: int, status: AidRequestStatus) -> AidRequestOut:
+        return await self.__repository.update_aid_request_status(db=db, aid_request_id=aid_request_id, status=status)
+
+    async def set_volunteer_deadline(self, db, aid_request_id: int, deadline: datetime.datetime) -> AidRequestOut:
+        return await self.__repository.set_volunteer_deadline(db=db, aid_request_id=aid_request_id, deadline=deadline)
+
+    async def delete_aid_request(self, db, aid_request_id: int, soldier_id: int) -> bool:
+        return await self.__repository.delete_aid_request(db=db, aid_request_id=aid_request_id, soldier_id=soldier_id)
+
+    async def reject_aid_request(self, db, aid_request_id: int, volunteer_id: int) -> bool:
+        return await self.__repository.reject_aid_request(db=db, aid_request_id=aid_request_id, volunteer_id=volunteer_id)
