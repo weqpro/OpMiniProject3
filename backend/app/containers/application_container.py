699f62b3a391c254c31470902f7facae03fe8b44
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-04-08 12:09:43 +0300

    review service in container

--- backend/app/containers/application_container.py
+++ backend/app/containers/application_container.py
@@ -1,9 +1,9 @@
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Container, Singleton
+from dependency_injector.providers import Container
 from .services import ServiceContainer
 from app.repository.repository_manager import RepositoryManager
 
 
 class ApplicationContainer(DeclarativeContainer):
     services = Container(ServiceContainer, repository=RepositoryManager())
-    review_service = Singleton(ReviewService)
+
