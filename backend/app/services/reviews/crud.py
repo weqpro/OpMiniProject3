01ebdc911366ed4ad85b5d8bd97e268bf21a8e13
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-04-08 11:42:04 +0300

    database access function

---
+++ backend/app/services/reviews/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def create_review(db: Session, review: schemas.ReviewCreate):
+    db_review = models.Review(**review.dict())
+    db.add(db_review)
+    db.commit()
+    db.refresh(db_review)
+    return db_review
+
+def get_reviews_by_volunteer(db: Session, volunteer_id: int):
+    return db.query(models.Review).filter(models.Review.volunteer_id == volunteer_id).all()
+
+def report_review(db: Session, review_id: int):
+    review = db.query(models.Review).filter(models.Review.id == review_id).first()
+    if review:
+        review.reported = True
+        db.commit()
+        db.refresh(review)
+    return review
