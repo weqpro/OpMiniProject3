41c936e55d78514d16a70b213ef4ad55dc9d0444
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-04-08 11:54:38 +0300

    container update

--- backend/app/containers/application_container.py
+++ backend/app/containers/application_container.py
@@ -1,8 +1,9 @@
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Container
+from dependency_injector.providers import Container, Singleton
 from .services import ServiceContainer
 from app.repository.repository_manager import RepositoryManager
 
 
 class ApplicationContainer(DeclarativeContainer):
     services = Container(ServiceContainer, repository=RepositoryManager())
+    review_service = Singleton(ReviewService)
