1c6ef14268c5b946781ed5f449d9c341fbc80d72
weqpro <weqpro@gmail.com>
2025-04-13 17:37:24 +0300

    fixed some errors with imports

--- backend/app/routers/v1/__init__.py
+++ backend/app/routers/v1/__init__.py
@@ -1,1 +1,1 @@
-from .aid_requests import router
+from .aid_requests import router as aid_requests_router
--- backend/app/services/review_service.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from backend.app import crud, models, schemas
-
-class ReviewService:
-    async def create_review(self, db: Session, review: schemas.review.ReviewCreate):
-        """
-        Create a new review in the database.
-        """
-        return crud.reviews.crud.create_review(db, review)
