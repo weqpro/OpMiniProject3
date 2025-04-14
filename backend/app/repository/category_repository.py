2157d8992d126b0a800c6ff7eaaa7f64faaac3a9
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-04-14 13:16:05 +0300

    category repository

--- backend/app/repository/category_repository.py
+++ backend/app/repository/category_repository.py
@@ -1,13 +1,16 @@
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 from typing import override
 from contextlib import AbstractAsyncContextManager
 
 from fastapi import Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.elements import ColumnElement
+from sqlalchemy.sql.expression import ColumnExpressionArgument
 
-from app.contracts import RepositoryBase
-from app.models import Category
-from app.repository import RepositoryContext, get_repository_context
+from app.contracts.repository_base import RepositoryBase
+from app.models.category import Category
+from app.repository.repository_context import RepositoryContext, get_repository_context
 
 
 class CategoryRepository(RepositoryBase[Category]):
@@ -17,6 +20,22 @@ class CategoryRepository(RepositoryBase[Category]):
             context.session_maker
         )
         super().__init__(session_maker, Category)
+
+    async def find(self, *order_by: ColumnElement | str) -> Sequence[Category]:
+        async with self._session_maker() as session:
+            stmt = select(self._model).order_by(*order_by)
+            result = await session.execute(stmt)
+            return result.scalars().all()
+
+    async def find_by_condition(
+        self,
+        condition: ColumnExpressionArgument[bool],
+        *order_by: ColumnElement | str,
+    ) -> Sequence[Category]:
+        async with self._session_maker() as session:
+            stmt = select(self._model).where(condition).order_by(*order_by)
+            result = await session.execute(stmt)
+            return result.scalars().all()
 
 
 async def get_category_repository(
