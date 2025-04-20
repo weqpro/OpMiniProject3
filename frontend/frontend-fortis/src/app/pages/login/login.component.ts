1c5b8d1ecd3652f6876cf4036ca867c6d0fdb81f
daryna prytula <darynkaprytula11@gmail.com>
2025-04-20 18:17:32 +0300

    added user role selection at login

--- frontend/frontend-fortis/src/app/pages/login/login.component.ts
+++ frontend/frontend-fortis/src/app/pages/login/login.component.ts
@@ -1,59 +1,55 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { RouterModule, Router } from '@angular/router';
-import { AuthService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   imports: [
-    CommonModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatMenuModule,
-    RouterModule
   ]
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  loading = false;
+  email: string = '';
+  password: string = '';
+  selectedRole: 'volunteer' | 'soldier' = 'volunteer';
+  baseUrl: string = 'http://localhost:8000/api/v1/token';
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private http: HttpClient, private router: Router) {}
+
+  selectRole(role: 'volunteer' | 'soldier') {
+    this.selectedRole = role;
+  }
 
   login() {
-    if (!this.email || !this.password) {
-      alert('Будь ласка, введіть email та пароль');
-      return;
-    }
+    const endpoint = `${this.baseUrl}/${this.selectedRole}`;
+    const body = {
+      username: this.email,
+      password: this.password
+    };
 
-    this.loading = true;
-    this.authService.login(this.email, this.password).subscribe({
+    this.http.post(endpoint, body).subscribe({
       next: (response: any) => {
-        localStorage.setItem('token', response.access_token);
-        alert('Успішний вхід!');
+        localStorage.setItem('access_token', response.access_token);
         this.router.navigate(['/requests-filter']);
-        this.loading = false;
       },
-      error: (err: any) => {
-        console.error('Помилка входу', err);
+      error: () => {
         alert('Невірний email або пароль');
-        this.loading = false;
       }
     });
+  }
+
+  goToRegister() {
+    this.router.navigate(['/role-selection']);
   }
 }
